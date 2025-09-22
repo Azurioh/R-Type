7e33074865a9903d90fecb130530a9671215ef15
Alan CUNIN <alan.cunin@epitech.eu>
2025-09-22 15:03:35 +0200

    refactor(common): remove comments in the Sprite class

--- common/include/Subsystems/Render/Sprite.hpp
+++ common/include/Subsystems/Render/Sprite.hpp
@@ -22,28 +22,10 @@ namespace Subsystems::Render {
             void setTexture(Texture *texture);
             void setSprite(void *sprite);
             void setColor(void *color);
-            // void setX(float x);
-            // void setY(float y);
-            // void setZ(float z);
-            // void setWidth(float width);
-            // void setHeight(float height);
-            // void setRotation(float rotation);
-            // void setScale(float scale);
-            // void setAngle(float angle);
-            // void setOriginX(float originX);
-            // void setOriginY(float originY);
-            // void setOriginZ(float originZ);
 
         private:
             Texture *_texture;
             void *_sprite;
             void *_color;
-            // float _x;
-            // float _y;
-            // float _z;
-            // float _width;
-            // float _height;
-            // float _rotation;
-            // float _scale;
     };
 }
