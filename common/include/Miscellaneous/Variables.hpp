4d0e1b2ad211bbde399f67066ecf27d5ad1c9982
Alan CUNIN <alan.cunin@epitech.eu>
2025-09-25 18:41:55 +0200

    refactor(common): remove the namespace for the variables

--- common/include/Miscellaneous/Variables.hpp
+++ common/include/Miscellaneous/Variables.hpp
@@ -9,17 +9,15 @@
 
 #include <cstdint>
 
-namespace RType {
-    /*!> The size of a pagination. */
-    constexpr std::uint8_t PAGINATION_SIZE = 5;
+/*!> The size of a pagination. */
+constexpr std::uint8_t PAGINATION_SIZE = 5;
 
-    /*!> Get the index of a pagination. */
-    constexpr std::uint8_t PAGINATION_IDX(std::uint8_t value) {
-        return value / PAGINATION_SIZE;
-    }
+/*!> Get the index of a pagination. */
+constexpr std::uint8_t PAGINATION_IDX(std::uint8_t value) {
+    return value / PAGINATION_SIZE;
+}
 
-    /*!> Get the index in a pagination. */
-    constexpr std::uint8_t IDX_IN_PAGINATION(std::uint8_t value) {
-        return value % PAGINATION_SIZE;
-    }
+/*!> Get the index in a pagination. */
+constexpr std::uint8_t IDX_IN_PAGINATION(std::uint8_t value) {
+    return value % PAGINATION_SIZE;
 }
